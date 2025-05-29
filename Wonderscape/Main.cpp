07734819adac45d2966c30ea7df5d28266ef1302
Rodríguez Díaz Axel Josué <rdaxel19@gmail.com>
2025-05-29 15:54:12 -0600

    Creación del Menú
    Fondo de este sin interacción, solo ubicación de los componentes en pantalla, junto con la creación de nuevos archivos para su correcta ejecución

--- Wonderscape/Main.cpp
+++ Wonderscape/Main.cpp
@@ -1,8 +1,30 @@
-#include"Model.h"
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
 
+#include"shaderClassmp.h"
+#include"VAOmp.h"
+#include"VBOmp.h"
+#include"EBOmp.h"
 
-const unsigned int width = 800;
-const unsigned int height = 800;
+//Coordenadas de Vertices
+GLfloat vertices[] =
+{
+	// COORDENADAS        	COLORES			COORDENADAS PARA LA TEXTURA
+	-1.0f, -1.0f, 0.0f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f, // Esq.Inf.Izq
+	-1.0f,  1.0f, 0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 1.0f, // Esq.Sup.Izq
+	 1.0f,  1.0f, 0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f, // Esq.Sup.Der
+	 1.0f, -1.0f, 0.0f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f, // Esq.Inf.Der
+};
+
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 2, 1, // Parte Superior del Triangulo
+	0, 3, 2, // Parte Inferior del Triangulo
+};
+
 
 
 int main()
@@ -18,8 +40,8 @@ int main()
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
+	// Create a GLFWwindow object of 800 by 800 pixels
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Wonderscape", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -34,54 +56,85 @@ int main()
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, width, height);
+	glViewport(0, 0, 1920, 1080);
+
+
+	// Generates Shader object using shaders defualt.vert and default.frag
+	Shadermp shaderProgram("defaultmp.vert", "defaultmp.frag");
 
 
 
+	// Generates Vertex Array Object and binds it
+	VAOmp VAO1;
+	VAO1.Bind();
+
+	// Generates Vertex Buffer Object and links it to vertices
+	VBOmp VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBOmp EBO1(indices, sizeof(indices));
+
+	// Links VBO attributes such as coordinates and colors to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	//Gets ID of uniform called "scale"
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	//Texture
+
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("Menu.jpg", &widthImg, &heightImg, &numColCh, 0);
 
 
-	// Generates Shader object using shaders default.vert and default.frag
-	Shader shaderProgram("default.vert", "default.frag");
 
-	// Take care of all the light related things
-	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
-	glm::mat4 lightModel = glm::mat4(1.0f);
-	lightModel = glm::translate(lightModel, lightPos);
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	//Para ubicar la textura 
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
 	shaderProgram.Activate();
-	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
-
-
-
-
-
-	// Enables the Depth Buffer
-	glEnable(GL_DEPTH_TEST);
-
-	// Creates camera object
-	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
-
-	// Load in a model
-	Model model("models/tierra/scene.gltf");
+	glUniform1i(tex0Uni, 0);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and depth buffer
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// Handles camera inputs
-		camera.Inputs(window);
-		// Updates and exports the camera matrix to the Vertex Shader
-		camera.updateMatrix(45.0f, 0.1f, 100.0f);
-
-		// Draw a model
-		model.Draw(shaderProgram, camera);
-
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		shaderProgram.Activate();
+		// Assigns a value to the uniform
+		shaderProgram.Activate();
+		//Assigns a value to the uniform
+		glUniform1f(uniID, 1.0f);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
@@ -91,6 +144,10 @@ int main()
 
 
 	// Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
